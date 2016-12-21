8e6809f7de190e92b94b64c46c38f91761dcc3b9
LouisMS <louisms@gmail.com>
2016-12-21 14:19:39 +0000

    final commit for this exercise

--- LMS_Wrangle_RefineOriginal.R
+++ LMS_Wrangle_RefineOriginal.R
@@ -34,13 +34,46 @@ glimpse(refine_original)
 # step 2: seperate refine_original$Product.code...number into 'product_code' and 'product_number'
 
 refine_original <- refine_original %>% 
-  separate(col = Product_code...number,into = c("product_code", "product_number"), sep = "-")
+  separate(col = Product.code...number,into = c("product_code", "product_number"), sep = "-")
 
 # step 3: add a column with the 'product_category' for each record, where:
 #         p = Smartphone, v = TV, x = Laptop, q = Tablet
 
-prod_cat_list <- c(p = "Smartphone", v = "TV", x = "Laptop", q = "Tablet")
+ProdCatFunc <- function(x) {
+  ifelse(x == "p", "Smartphone",
+         ifelse(x == "v", "TV",
+                ifelse(x == "x", "Laptop",
+                       ifelse(x == "q", "Tablet", NA))))
+}
 
-refine_original <- refine_original %>% mutate(product_code, prod_cat_list)
+refine_original <- refine_original %>% mutate(product_category = ProdCatFunc(product_code))
 
-names(refine_original)
+# step 4: condense the 3 address columns 'address', 'city' and 'country' into one new column
+#         called 'full_address'
+
+refine_original <- refine_original %>% unite(full_address, address:country, sep = ",", remove = FALSE)
+
+# step 5: create dummy variables for company and product categories using the prefixes 'company_'
+#         and 'product_'
+
+# 'company_'
+refine_original <- refine_original %>%
+  mutate(company_philips = ifelse(company == "philips", 1, 0)) %>%
+  mutate(company_akzo = ifelse(company == "akzo", 1, 0)) %>%
+  mutate(company_van_houten = ifelse(company == "van houten", 1, 0)) %>%
+  mutate(company_unilever = ifelse(company == "unilever", 1, 0))
+
+# 'product_'
+refine_original <- refine_original %>%
+  mutate(product_smartphone = ifelse(product_category == "Smartphone", 1, 0)) %>%
+  mutate(product_tv = ifelse(product_category == "TV", 1, 0)) %>%
+  mutate(product_laptop = ifelse(product_category == "Laptop", 1, 0)) %>%
+  mutate(product_tablet = ifelse(product_category == "Tablet", 1, 0))
+
+glimpse(refine_original)
+
+# generate new file called 'refine_clean'
+
+refine_clean <- refine_original
+
+write.csv(refine_clean, file = "refine_clean.csv")
--- make a DF.R
+++ make a DF.R
@@ -5,10 +5,6 @@ x
 y <- data.frame(x = 1:6, y = paste0(letters[1:6], "-", 1:6))
 y
 
-y
-separate(data = y,col = y, into = c("a", "b") sep = "-")
-
-
 y %>% separate(col = y, into = c("letter", "number"))
 
 glimpse(y)
